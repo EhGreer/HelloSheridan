0f1ac5d3486decc2e72a6315c99686787c9f9813
Alexey Greer <greerale@sheridancollege.ca>
2021-05-20 13:47:48 -0400

    Created the empty project

---
+++ src/test/java/sheridan/greer/hellosheridan/HelloSheridanApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.greer.hellosheridan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSheridanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
